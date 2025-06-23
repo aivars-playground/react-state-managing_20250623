d55cf109860d97b8b70c4041780e00f41ed17b0a
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-23 15:38:39 +0100

    adding pages

--- wip_03_before/src/Detail.jsx
+++ wip_03_before/src/Detail.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 
 export default function Detail() {
-    return <h1>Detail</h1>;
-
-    // TODO: Display these products details
-    // return (
-    //   <div id="detail">
-    //     <h1>{product.name}</h1>
-    //     <p>{product.description}</p>
-    //     <p id="price">${product.price}</p>
-    //     <img src={`/images/${product.image}`} alt={product.category} />
-    //   </div>
-    // );
+    return (
+      <div id="detail">
+          ddd
+        {/*<h1>{product.name}</h1>*/}
+        {/*<p>{product.description}</p>*/}
+        {/*<p id="price">${product.price}</p>*/}
+        {/*<img src={`/images/${product.image}`} alt={product.category} />*/}
+      </div>
+    );
 }
