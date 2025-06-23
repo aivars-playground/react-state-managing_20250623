fbda4e624509819116fad6827c9166bb736c3f5d
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-23 16:08:16 +0100

    add to cart

--- wip_03_before/src/Detail.jsx
+++ wip_03_before/src/Detail.jsx
@@ -3,10 +3,12 @@ import {useParams} from "react-router-dom";
 import useFetch from "./services/useFetch";
 import Spinner from "./Spinner";
 import PageNotFound from "./PageNotFound.";
+import {useNavigate} from "react-router-dom";
 
 export default function Detail() {
 
     const {id} = useParams();
+    const navigate = useNavigate();
 
     const { data: product, loading, error } = useFetch(
         `products/${id}`
@@ -21,6 +23,11 @@ export default function Detail() {
         <h1>{product.name}</h1>
         <p>{product.description}</p>
         <p id="price">${product.price}</p>
+        <p>
+            <button className="btn btn-primary" onClick={()=> navigate("/cart")}>
+                add to cart
+            </button>
+        </p>
         <img src={`/images/${product.image}`} alt={product.category} />
       </div>
     );
