502b215a9160e2b454b0c4a55a3024ca1e696af6
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-23 15:53:21 +0100

    adding details

--- wip_03_before/src/Detail.jsx
+++ wip_03_before/src/Detail.jsx
@@ -1,13 +1,27 @@
 import React from "react";
+import {useParams} from "react-router-dom";
+import useFetch from "./services/useFetch";
+import Spinner from "./Spinner";
+import PageNotFound from "./PageNotFound.";
 
 export default function Detail() {
+
+    const {id} = useParams();
+
+    const { data: product, loading, error } = useFetch(
+        `products/${id}`
+    );
+
+    if (error) throw error;
+    if (loading) return <Spinner />;
+    if (product.length === 0) return <PageNotFound />;
+
     return (
       <div id="detail">
-          ddd
-        {/*<h1>{product.name}</h1>*/}
-        {/*<p>{product.description}</p>*/}
-        {/*<p id="price">${product.price}</p>*/}
-        {/*<img src={`/images/${product.image}`} alt={product.category} />*/}
+        <h1>{product.name}</h1>
+        <p>{product.description}</p>
+        <p id="price">${product.price}</p>
+        <img src={`/images/${product.image}`} alt={product.category} />
       </div>
     );
 }
