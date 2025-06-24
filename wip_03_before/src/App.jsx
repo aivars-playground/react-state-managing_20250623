a85153c4be4136d7bbfed97a6c3fc924b9eb8c53
Aivars Smaukstelis <aivars.smaukstelis@gmail.com>
2025-06-24 13:00:51 +0100

    adding localStorage state management

--- wip_03_before/src/App.jsx
+++ wip_03_before/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
 import Footer from "./Footer";
 import Header from "./Header";
@@ -10,7 +10,19 @@ import Cart from "./Cart";
 
 export default function App() {
 
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("cart")) ?? [];
+    } catch {
+      console.error("The cart could not be parsed into JSON.");
+      return [];
+    }
+  });
+
+  useEffect(
+    () => localStorage.setItem("cart", JSON.stringify(cart)),
+    [cart]
+  );
 
   function addToCart(id, sku) {
     setCart(items => {
